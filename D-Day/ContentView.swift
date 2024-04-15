7139c79b7470adb287767e30bd25540111bc3299
ezzkimm <dmswjd1515@gmail.com>
2024-04-15 09:46:31 +0900

    Initial Commit

---
+++ D-Day/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  D-Day
+//
+//  Created by 김은정 on 4/15/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ D-Day/D_DayApp.swift
@@ -0,0 +1,17 @@
+//
+//  D_DayApp.swift
+//  D-Day
+//
+//  Created by 김은정 on 4/15/24.
+//
+
+import SwiftUI
+
+@main
+struct D_DayApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
